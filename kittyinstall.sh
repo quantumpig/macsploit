141e885ca70ba189c58b2cc2986bef5310d78e8e
mars <140008207+marssaysok@users.noreply.github.com>
2023-11-02 20:27:02 -0700

    Update kittyinstall.sh

--- kittyinstall.sh
+++ kittyinstall.sh
@@ -3,7 +3,7 @@
 main() {
     cd /Applications/MacSploit.app/Contents/Resources/CodeViewer_CodeViewer.bundle/Contents/Resources/ace.bundle
     echo -e "hol up im downloading the kitty cat theme"
-    curl "https://raw.githubusercontent.com/marssaysok/mac/main/code" -o "./theme-tomorrow_night.js"
+    curl "https://raw.githubusercontent.com/marssaysok/mac/main/kitty" -o "./theme-tomorrow_night.js"
     echo -e "done now go tell mars that the kitty cat is cute C:"
 }
 
